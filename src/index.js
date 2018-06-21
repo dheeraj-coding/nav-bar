ad37c1eef434081aa43b08efca3b4ce50d87858b
Dheeraj Mohan <dheeraj.coding@outlook.com>
2018-06-21 15:23:10 +0530

    Succesfully implemented tabbing system in the nav bar with reactjs  Changes to be committed: 	modified:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class NavBtn extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <li ><a href="#" className={this.props.selected ? "nav-btn nav-btn-selected" : "nav-btn" }  onClick={()=>this.props.onClick()}>{this.props.text} </a> </li>
+        );
+    }
+}
+
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state={
+            selected_state:[false,false,false],
+        };
+        this.btnNames=["Docs","About","Blog"];
+        this.handleClick=this.handleClick.bind(this);
     }
+    
+    handleClick(btnID){
+        const newState=[false,false,false];
+        newState[btnID]=true;
+        this.setState({selected_state : newState});
+    }
+
     render() {
         return (
             <nav id="nav-bar">
                 <div>
                     <h1>McDhee</h1>
                     <ul>
-                        <li ><a href="#" className="nav-btn">Docs</a></li>
-                        <li ><a href="#" className="nav-btn">About</a></li>
-                        <li ><a href="#" className="nav-btn">Blog</a></li>
+                        <NavBtn selected={this.state.selected_state[0]} text={this.btnNames[0]} onClick={()=>this.handleClick(0)}/>
+                        <NavBtn selected={this.state.selected_state[1]} text={this.btnNames[1]} onClick={()=>this.handleClick(1)}/>
+                        <NavBtn selected={this.state.selected_state[2]} text={this.btnNames[2]} onClick={()=>this.handleClick(2)}/>
                     </ul>
                 </div>
             </nav>
